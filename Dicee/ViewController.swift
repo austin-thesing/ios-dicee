61e59329a59100648d8cb1de6585947c80441631
Austin Thesing <austin@designxdevelop.com>
2018-06-09 00:21:37 -0700

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Austin Thesing on 6/9/18.
+//  Copyright © 2018 Austin Thesing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
